80978548ccb57d2d64a64cfe5d8364415d95ebe9
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-07 22:12:15 -0600

    Edited capitilization and stylesheet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script setup>
 import {ref} from 'vue';
 
+const timeInput = ref("");
+const timeLeft = ref(null);
+const countdown = ref(null);
+
+const startCountdown = () => {
+  const time = parseInt(timeInput.value, 10);
+  if (!isNaN(time) && time >= 1 && time <= 60) {
+    timeLeft.value = time;
+    clearInterval(countdown.value);
+    countdown.value = setInterval(() => {
+      if (timeLeft.value > 0) {
+        timeLeft.value--;
+      } else {
+        clearInterval(countdown.value);
+        alert('Time is up!');
+        timeLeft.value = null;
+      }
+    }, 1000);
+  } else {
+    alert('Please enter a valid number between 1 and 60.');
+  }
+};
+
 </script>
 
 <template>
@@ -10,16 +33,16 @@ import {ref} from 'vue';
       <h1>Countdown Timer</h1>
 
       <div class="content">
-        <label for="timeInput">Enter time (in seconds):</label>
+        <label for="timeInput">Enter Time (In Seconds):</label>
         <input type="number" v-model="timeInput" max="60" min="1" placeholder="Seconds" />
       </div>
 
       <button @click="startCountdown" class="start-button">Start Countdown</button>
 
-      <div class="timer">
-
-
+      <div class="timer" v-if="timeLeft !== null">
+        {{ timeLeft }} seconds
       </div>
+
     </div>
   </main>
 </template>
